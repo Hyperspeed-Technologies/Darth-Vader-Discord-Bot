1dc8ec3d702998db951b7866cff93a1315a5808b
Francisco <frangc0206@gmail.com>
2021-10-24 20:04:28 -0300

    Down Detector Feature In progress

---
+++ features/downDetector.ts
@@ -0,0 +1,14 @@
+import { Client, GuildMember } from 'discord.js';
+
+export default (client: Client) => {
+    client.on('guildMemberUpdate', async (oldMember, newMember) => {
+        console.log(newMember.presence)
+    })
+}
+
+const config = {
+    displayName: 'Bot down detector',
+    dbName: 'EXTRA_BOT_DOWN_DETECTOR'
+}
+
+export { config }
