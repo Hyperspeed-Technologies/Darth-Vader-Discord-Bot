28e0d3b11163ea03b2ae0af41c81450672ddbdba
Francisco <frangc0206@gmail.com>
2021-10-26 19:55:07 -0300

    Disabled feature

--- features/downDetector.ts
+++ features/downDetector.ts
@@ -2,7 +2,7 @@ import { Client, GuildMember } from 'discord.js';
 
 export default (client: Client) => {
     client.on('guildMemberUpdate', async (oldMember, newMember) => {
-        console.log(newMember.presence)
+        // console.log(newMember.presence)
     })
 }
 
