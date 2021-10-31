fdf5a6f6ca2bd0ad442cb486326f510a019553ef
Francisco <frangc0206@gmail.com>
2021-10-31 01:33:56 -0300

    Added "DIRECT_MESSAGES" Intent

--- index.ts
+++ index.ts
@@ -9,7 +9,8 @@ const client = new DiscordJS.Client({
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILD_MEMBERS
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.DIRECT_MESSAGES
     ]
 })
 
